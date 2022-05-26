6960de777daa019bd3fc69520e3291fcde8723c2
AhmadJabrayilov <tu25tyfpp@code.edu.az>
2022-05-27 01:11:56 +0400

    featured task

---
+++ FrontToBack103/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FrontToBack103.DAL;
+using FrontToBack103.Models;
+using FrontToBack103.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrontToBack103.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Index()
+        {
+            HomeVM homeVM = new HomeVM();
+            homeVM.sliders = _context.Sliders.ToList();
+            homeVM.PageIntro = _context.PageIntros.FirstOrDefault();
+            homeVM.products = _context.Products.Include(p=>p.Category).ToList();
+            homeVM.categories = _context.Categories.ToList();
+
+            return View(homeVM);
+        }
+    }
+}
